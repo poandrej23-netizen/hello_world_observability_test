aa479c3a19b0eebc1abe34cfbc2c3a211a3a8504
poandrej23-netizen <po.andrej23@gmail.com>
2026-05-15 19:13:36 +0300

    Update generate-load.sh

--- scripts/generate-load.sh
+++ scripts/generate-load.sh
@@ -4,8 +4,8 @@
 
 set -e
 
-NAMESPACE="${1:-hello_world_app}"
-SERVICE="${2:-hello_world_app}"
+NAMESPACE="${1:-hello-app}"
+SERVICE="${2:-hello-app}"
 DURATION="${3:-60}"  # секунд
 
 echo "Generating load for ${SERVICE}.${NAMESPACE} for ${DURATION}s..."
